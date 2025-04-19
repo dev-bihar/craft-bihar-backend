e7a8978753fe4498a444b385b313ef04be2ed529
shivam.jha1 <shivam.jha@pw.live>
2025-04-20 02:20:16 +0530

    Update healthcheck test in AppController to reflect new API response

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -15,8 +15,8 @@ describe('AppController', () => {
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+    it('should return "Craft Bihar API"', () => {
+      expect(appController.healthcheck()).toBe('Craft Bihar API');
     });
   });
 });
